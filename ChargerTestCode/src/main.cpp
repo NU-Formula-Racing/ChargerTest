8521a6e5e1e41b5a83de1fe66b9a29ac615d16b1
izo0619 <isabelzhong30@gmail.com>
2024-06-10 23:25:41 -0500

    sketchy changes to fix timing bugs

--- ChargerTestCode/src/main.cpp
+++ ChargerTestCode/src/main.cpp
@@ -12,46 +12,51 @@ TeensyCAN<1> can_bus{};
 // The tx and rx pins are constructor arguments to ESPCan, which default to TX = 5, RX = 4
 // ESPCAN can_bus{};
 
-ElconCharger charger(can_bus);
+ElconCharger charger(can_bus, 3300, 14);
 // Make a VirtualTimerGroup to add your timers to
 VirtualTimerGroup timer_group{};
 
-void ten_ms_task()
+void test()
 {
-  /**
-   * You can use and set the CANSignals as if they were SignalType
-   *
-   */
-  // float_tx_signal = 5.195f;
-  // uint8_t_tx_signal = 132;
-  // bool_tx_signal = true;
-  // millis_tx_signal = millis();
+    /**
+     * You can use and set the CANSignals as if they were SignalType
+     *
+     */
+    // float_tx_signal = 5.195f;
+    // uint8_t_tx_signal = 132;
+    // bool_tx_signal = true;
+    // millis_tx_signal = millis();
 
-  // The CANRXMessage automatically gets updated on message reception from the interrupt. Not: in order for this to
-  // work, you must periodically call Tick() on the can_bus
-  charger.Tick(millis());
-  can_bus.Tick();
-  Serial.print("Received output voltage: ");
-  Serial.println(charger.GetOutputVoltage());
-  Serial.print("Received output current: ");
-  Serial.println(charger.GetOutputCurrent());
-  Serial.print("Received power reference: ");
-  Serial.println(charger.GetPower());
-  Serial.print("Received available power: ");
-  Serial.println(charger.GetAvailablePower());
+    // The CANRXMessage automatically gets updated on message reception from the interrupt. Not: in order for this to
+    // work, you must periodically call Tick() on the can_bus
+    charger.Tick(millis());
+    can_bus.Tick();
+    Serial.print("Received output voltage: ");
+    Serial.println(charger.GetOutputVoltage());
+    Serial.print("Received output current: ");
+    Serial.println(charger.GetOutputCurrent());
+    Serial.print("Received power reference: ");
+    Serial.println(charger.GetPower());
+    Serial.print("Received available power: ");
+    Serial.println(charger.GetAvailablePower());
+    Serial.print("Received status: ");
+    Serial.println(charger.GetFaults().all_flags, BIN);
+    Serial.print("Received connected: ");
+    Serial.println(charger.IsConnected());
 }
 
 void setup()
 {
-  can_bus.Initialize(ICAN::BaudRate::kBaud500K);
+    can_bus.Initialize(ICAN::BaudRate::kBaud500K);
+    charger.Initialize();
 
-  // You can create a new timer in a VirtualTimerGroup using the AddTimer(function, time) function
-  timer_group.AddTimer(100, ten_ms_task);
-  Serial.begin(9600);
-  Serial.println("Started");
+    // You can create a new timer in a VirtualTimerGroup using the AddTimer(function, time) function
+    timer_group.AddTimer(100, test);
+    Serial.begin(9600);
+    Serial.println("Started");
 }
 
 void loop()
 {
-  timer_group.Tick(millis());
+    timer_group.Tick(millis());
 }
